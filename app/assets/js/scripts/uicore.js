08adafdf090ca441361abe42e11d7a7e3fefdb86
Liam Robinson <liamrobinson5@web.de>
2024-03-26 00:40:40 +0100

    Show UpdateUI as soon as possible

--- app/assets/js/scripts/uicore.js
+++ app/assets/js/scripts/uicore.js
@@ -54,6 +54,7 @@ if(!isDev){
                 }
                 
                 populateSettingsUpdateInformation(info)
+                showUpdateUI(info)
                 break
             case 'update-downloaded':
                 loggerAutoUpdater.info('Update ' + info.version + ' ready to be installed.')
